8262316d939a25e9f19459ae5d129e2c7bd0aede
IsiTapier <41296026+IsiTapier@users.noreply.github.com>
2020-10-28 10:56:29 +0100

    V.6.0

--- Final_V1/Final_V1.ino
+++ Final_V1/Final_V1.ino
@@ -1,37 +1,79 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_ST7735.h>
 
-#define D_L 128
-#define D_W 64
-#define arrayLength D_L
+//variabel defines:
+#define ROTATION 45
+#define DEBUG true
 
-#define ledRed 9
-#define ledGreen 10
-#define ledBlue 11
-#define inputGasSensor A0
-#define outsideValueSensor 200
-#define outsideValuePpm 400
-#define maxPpm 5000
-#define maxIncrease 1.5
-#define maxDecrease 10
-#define maxDropIncrease 50
-#define averaging 100
-#define maxLight 1000
-#define maxBlink 1100
-#define maxPiep 1200
-#define maxDisplayed 1400
-#define averagingGradient 5
-#define alpha 0.7
-#define debug true
-#define cycleLength 10*1000
+#define STAGE_TIME 1*1000
+  #define VENTILATING_TIMEOUT 5*1000
 
-#define criticalHight map(maxLight, 0, maxDisplayed, 0, D_W)
-#define minHight map(outsideValuePpm, 0, maxDisplayed, 0, D_W)
+#define AVERAGING_MEASUREMENTS 100
+#define AVERAGING_GRADIENT 5
+  #define ALPHA_MEASUREMENTS 0.7
+  #define ALPHA_LOWEST 0.5
 
-short graphData[arrayLength];
-Adafruit_SSD1306 display = Adafruit_SSD1306(D_L, D_W, &Wire, -1); // Create display object
+  #define OSV_SENSOR 80
+  #define OSV_PPM 400
+#define MIN_HIGHT DISPLAY_WIDTH - map(OSV_PPM, 0, MAX_DISPLAYED_PPM, 0, DISPLAY_WIDTH)
+#define CRITICAL_HIGHT DISPLAY_WIDTH - map(MAX_LIGHT, 0, MAX_DISPLAYED_PPM, 0, DISPLAY_WIDTH)
 
+  #define MAX_SENSOR 1023
+  #define MAX_PPM 5000
+  #define MAX_INCREASE 1.5
+  #define MAX_DECREASE 10
+#define MAX_DROP_INCREASE 50
+#define MAX_LIGHT 1000
+#define MAX_BLINK 1100
+#define MAX_PIEP 1200
+#define MAX_DISPLAYED_PPM 1280
+
+//pins:
+#define TFT_CS     10
+#define TFT_RST    9
+#define TFT_DC     8
+#define LED_RED 5
+#define LED_GREEN 6
+#define LED_BLUE 7
+#define GAS_SENSOR A0
+
+//Data:
+#define DISPLAY_LENGTH 159
+#define DISPLAY_WIDTH 127
+
+//Design
+#define DATABOX_TOP_HIGHT 100
+#define GRAPH_COLOR WHITE//MAGENTA
+#define GRAPH_BACKGROUND_COLOR BLACK
+#define BAR_BACKGROUND_COLOR GREY
+#define BAR_STRIPE_THICKNESS 3
+
+// Color definitions
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
+#define GREY 0x8C51
+#define LIME 0x87F4
+
+#define PPM_COLOR_N BLACK //Normal
+#define PPM_COLOR_R 0xFE0E //Risk
+#define PPM_COLOR_A 0xF800 //Alarm
+
+
+Adafruit_ST7735 display = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+short graphData[DISPLAY_LENGTH];
+unsigned long startTime;
+unsigned long timer;
+String lastTime;
+String Time;
+short seconds;
+short minutes;
 int airCondition = 0;
 short lastAirCondition = 0;
 short airConditionRaw = 0;
@@ -40,12 +82,16 @@ short lowest = 600;
 short led;
 short red;
 short green;
-short values[averagingGradient * 2];
+short values[AVERAGING_GRADIENT * 2];
 short last;
 short now;
-short gradient;
+short pitch;
 short drop;
 boolean ventilating = false;
+short airStatus;
+short lastAirConditionGraph = 0;
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -57,105 +103,111 @@ void loop() {
   loopSensor();
 }
 
+void debug(String title, int value) {
+  Serial.print(title + ": ");
+  Serial.println(value);
+}
+
+int average(int averageArray[], int averageStart, int averageEnd) {
+  int back = 0;
+  for (int i = averageStart; i < averageEnd; i++) {
+    back = back + averageArray[i];
+  }
+  return (back / (averageEnd - averageStart));
+}
+
 
 //Sensor
 
 void initSensor() {
   //Pins
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledGreen, OUTPUT);
-  pinMode(ledBlue, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
 
-  airConditionRaw = analogRead(inputGasSensor);
-  lastAirCondition = analogRead(inputGasSensor);
+  airConditionRaw = analogRead(GAS_SENSOR);
+  lastAirCondition = analogRead(GAS_SENSOR);
+  startTime = millis();
 }
 
 void loopSensor() {
   meassureAirCondition();
   mapAirCondition();
-  calculategradient();
+  calculatePitch();
   checkVentilating();
+  setStatus();
   writeLed();
+  draw(map(airCondition, 0, MAX_DISPLAYED_PPM, 0, DISPLAY_WIDTH));
   debugSensor();
 }
 
 void debugSensor() {
-  if (debug) {
+  if (DEBUG) {
     Serial.println("");
     Serial.println("Sensor");
     Serial.println("");
-    Serial.print("Analog: ");
-    Serial.println(analogRead(inputGasSensor));
-    Serial.print("Average: ");
-    Serial.println(airConditionRaw);
-    Serial.print("Smoothed: ");
-    Serial.println(lastAirCondition);
-    Serial.print("PPM: ");
-    Serial.println(airCondition);
-    Serial.print("gradient: ");
-    Serial.println(gradient);
-    Serial.print("Lowest: ");
-    Serial.println(lowest);
+    debug("Analog", analogRead(GAS_SENSOR));
+    debug("Average", airConditionRaw);
+    debug("Smoothed", lastAirCondition);
+    debug("PPM", airCondition);
+    debug("Pitch", pitch);
+    debug("Lowest", lowest);
+    debug("Status", airStatus);
   }
 }
 
 void meassureAirCondition() {
   //Messung
   airCondition = 0;
-  for (int i = 0; i < averaging; i++) {
-    value = analogRead(inputGasSensor);
+  for (int i = 0; i < AVERAGING_MEASUREMENTS; i++) {
+    value = analogRead(GAS_SENSOR);
 
     //Fehlmessungen überschreiben
-    if (airConditionRaw * maxIncrease < value && i == 0)
+    if (airConditionRaw * MAX_INCREASE < value && i == 0)
       airCondition = airCondition + airConditionRaw;
-    else if (airCondition / i * maxIncrease < value && i != 0)
+    else if (airCondition / i * MAX_INCREASE < value && i != 0)
       airCondition = airCondition + airCondition / i;
     else
       airCondition = airCondition + value;
 
-    delay(cycleLength/averaging);
+    delay(STAGE_TIME / AVERAGING_MEASUREMENTS);
   }
-  
-  airCondition = airCondition / averaging;
+
+  airCondition = airCondition / AVERAGING_MEASUREMENTS;
   airConditionRaw = airCondition;
 
   //Wert smoothen;
-  airCondition = alpha * airCondition + (1 - alpha) * lastAirCondition;
+  airCondition = ALPHA_MEASUREMENTS * airCondition + (1 - ALPHA_MEASUREMENTS) * lastAirCondition;
 
   lastAirCondition = airCondition;
-  
+
 }
 
 void mapAirCondition() {
   //to PPM
-  airCondition = airCondition - outsideValueSensor;
-  airCondition = map(airCondition, 0, 1023, outsideValuePpm, maxPpm);
+  if (airCondition < OSV_SENSOR)
+    airCondition = OSV_SENSOR;
+
+  airCondition = airCondition - OSV_SENSOR;
+  airCondition = map(airCondition, 0, MAX_SENSOR, OSV_PPM, MAX_PPM);
 
   //to Graph
-  draw(map(airCondition, 0, maxDisplayed, 0, D_W));
+
 }
 
-void calculategradient() {
+void calculatePitch() {
   //store last AirConditions
-  for (int i = averagingGradient * 2 - 1; i > 0; i--) {
+  for (int i = AVERAGING_GRADIENT * 2 - 1; i > 0; i--) {
     values[i] = values[i - 1];
   }
   values[0] = airCondition;
 
-  //average current AirCondition
-  for (int i = 0; i < averagingGradient; i++) {
-    now = now + values[i];
-  }
-  now = now / averagingGradient;
+  //average AirConditions
+  now = average(values, 0, AVERAGING_GRADIENT);
+  last = average(values, AVERAGING_GRADIENT, AVERAGING_GRADIENT * 2);
 
-  //average last AirCondition
-  for (int i = averagingGradient; i < averagingGradient * 2; i++) {
-    last = last + values[i];
-  }
-  last = last / averagingGradient;
-
-  //gradient
-  gradient = now - last;
+  //Pitch
+  pitch = now - last;
 }
 
 void checkVentilating() {
@@ -164,30 +216,31 @@ void checkVentilating() {
     drop = airCondition;
 
   //start Ventilating
-  if (gradient * -1 > maxDecrease && !ventilating) {
+  if (pitch * -1 > MAX_DECREASE && !ventilating) {
     ventilating = true;
     drop = airCondition;
     rgb(0, 0, 255);
+    startTime = millis();
   }
 
   //stop Ventilating
-  if (last < now && ventilating) {
-    if (drop - lowest < maxDropIncrease)
+  if (last < now && ventilating || millis() - timer >= VENTILATING_TIMEOUT && ventilating && timer != 0) {
+    if (drop - lowest < MAX_DROP_INCREASE) {
+      drop = ALPHA_LOWEST * drop + (1 - ALPHA_LOWEST) * lowest;
       lowest = drop;
+    }
     ventilating = false;
+    timer = 0;
+    startTime = millis();
+  } else if (now == 0 && timer == 0 && ventilating) {
+    timer = millis();
   }
   last = now;
 }
 
 void writeLed() {
-  //prepare Values
-  /*if (airCondition > maxLight)
-    airCondition = maxLight;
-  if (airCondition < lowest)
-    airCondition = lowest;*/
-
   //map Values
-  led = map(airCondition, lowest, maxLight, 0, 255);
+  led = map(airCondition, lowest, MAX_LIGHT, 0, 255);
   red = led;
   green = 255 - led;
 
@@ -197,9 +250,22 @@ void writeLed() {
 }
 
 void rgb(int red, int green, int blue) {
-  analogWrite(ledRed, red);
-  analogWrite(ledGreen, green);
-  analogWrite(ledBlue, blue);
+  analogWrite(LED_RED, red);
+  analogWrite(LED_GREEN, green);
+  analogWrite(LED_BLUE, blue);
+}
+
+void setStatus() {
+  airStatus = map(airCondition, lowest, MAX_LIGHT, 0, 2);
+  if (airCondition > MAX_PIEP)
+    airStatus = 4;
+  else if (airCondition > MAX_BLINK)
+    airStatus = 3;
+
+  if (airStatus < 0)
+    airStatus = 0;
+  if (airStatus > 4)
+    airStatus = 4;
 }
 
 
@@ -207,75 +273,138 @@ void rgb(int red, int green, int blue) {
 //Display
 
 void initDisplay() {
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("Error"));
-    for (;;);
-  }
-  display.clearDisplay();
+  display.initR(INITR_BLACKTAB);
+  display.setTextWrap(false);
+  display.setRotation(ROTATION);
+  display.fillScreen(GRAPH_BACKGROUND_COLOR);
 
   //Auffüllen des Arrays
-  for (short x = 0; x < arrayLength; x++) {
-    graphData[x] = 0;
+  for (short x = 0; x < DISPLAY_LENGTH; x++) {
+    graphData[x] = DISPLAY_LENGTH;
   }
+  graphData[0] = DATABOX_TOP_HIGHT - 1;
 
+  // Draw Bar Background
+  display.fillRect(0, DATABOX_TOP_HIGHT + 1, DISPLAY_LENGTH, DISPLAY_WIDTH, BAR_BACKGROUND_COLOR);
 }
 
 void createLines() {
-  drawLineMax();
-  drawLineMin();
-  display.display();
+  drawLine(DISPLAY_LENGTH + 1, CRITICAL_HIGHT, 5);
+  drawLine(DISPLAY_LENGTH + 1, MIN_HIGHT, 5);
+  writeInfo();
 }
 
-void drawLineMax() {
-  for (short x = arrayLength; x >= 0; x--) {
-    if ((x % 5) == 0) {
-      display.drawPixel(x, criticalHight, WHITE);
-    }
-  }
-}
-
-void drawLineMin() {
-  for (short x = arrayLength; x >= 0; x--) {
-    if ((x % 5) == 0) {
-      display.drawPixel(x, minHight, WHITE);
-    }
+void drawLine(int x, int y, int z) {
+  for (int i = 0; i < x; i = i + z) {
+    display.drawPixel(i, y, WHITE);
   }
 }
 
 void draw(int data) {
-  display.clearDisplay();
+  createLines();
   fillData(data);
   drawGraph();
-  createLines();
-  //writeVar(); //verdrehte Schrift
 }
 
 void drawGraph() {
-  for (short x = 0; x < arrayLength; x++) {
-    display.drawPixel(x, graphData[x], WHITE);
-
-    if (x != 0) {
-      if (graphData[x - 1] < graphData[x]) {
-        for (byte y = graphData[x]; y > graphData[x - 1]; y--) {
-          if (display.getPixel(graphData[x - 1], y) == 0) {
-            display.drawPixel(x, y, WHITE);
-          }
-        }
-      }
-      if (graphData[x - 1] > graphData[x]) {
-        for (byte y = graphData[x]; y < graphData[x - 1]; y++) {
-          //if (display.getPixel(graphData[x - 1], y) == 0) {
-          display.drawPixel(x, y, WHITE);
-          //}
-        }
-      }
+  for (short x = DISPLAY_LENGTH; x > 0; x--) {
+    byte arrayDigit = DISPLAY_LENGTH - x;
+    if (graphData[arrayDigit] < DATABOX_TOP_HIGHT)
+      display.drawPixel(x, graphData[arrayDigit], GRAPH_COLOR);
+    if (x != DISPLAY_LENGTH) {
+      if (graphData[arrayDigit - 1] < graphData[arrayDigit])
+        drawConnections(x, graphData[arrayDigit - 1] + 1, graphData[arrayDigit] + 1);
+      else
+        drawConnections(x, graphData[arrayDigit], graphData[arrayDigit - 1]);
     }
   }
 }
 
-void fillData(int data) {  // Künstliches Auffüllen der Werte, wird später vom Modul übernommen
-  for (short x = arrayLength - 1; x > 0; x--) {
+void drawConnections(int x, int startY, int endY) {
+  for (byte y = startY; y < endY; y++) {
+    if (y < DATABOX_TOP_HIGHT) {
+      display.drawPixel(x + 1, y, GRAPH_BACKGROUND_COLOR);
+      display.drawPixel(x, y, GRAPH_COLOR);
+    }
+  }
+}
+
+// Künstliches Auffüllen der Werte, wird später vom Modul übernommen
+void fillData(int data) {
+  for (short x = DISPLAY_LENGTH - 1; x > 0; x--) {
     graphData[x] = graphData[x - 1];
   }
-  graphData[0] = data;
+  graphData[0] = DISPLAY_WIDTH - data;
+}
+
+void writeInfo() {
+  //acertain ppm color
+  int currentColor = BLACK;
+  switch (airStatus) {
+    case 0: currentColor = GREEN;//PPM_COLOR_N;
+      break;
+    case 1: currentColor = YELLOW; //PPM_COLOR_R;
+      break;
+    case 2: currentColor = RED; //PPM_COLOR_A;
+      break;
+    default: currentColor = RED;
+      break;
+  }
+
+  //ppm zeichnen
+  if (ventilating)
+    display.fillRect(0, DATABOX_TOP_HIGHT + 1, DISPLAY_LENGTH, BAR_STRIPE_THICKNESS, CYAN);
+  else
+    display.fillRect(0, DATABOX_TOP_HIGHT + 1, DISPLAY_LENGTH, BAR_STRIPE_THICKNESS, currentColor);
+
+  //Verhindert überschreiben von "ppm"
+  if (airCondition < 1000)
+    dPrint(57, 118, 1.5, currentColor, "ppm");
+  else
+    dPrint(57, 118, 1.5, BAR_BACKGROUND_COLOR, "ppm");
+
+  //Clear old Pixels
+  dPrint(2, 105, 3, BAR_BACKGROUND_COLOR, lastAirConditionGraph);
+  //write new Pixels
+  dPrint(2, 105, 3, currentColor, airCondition);
+  //Set new lastAirCondition
+  lastAirConditionGraph = airCondition; //Setzt letzten Wert
+
+  //calculate time since last ventilating
+  seconds = (millis() - startTime) / 1000 % 60;
+  minutes = ((millis() - startTime) / 1000 - seconds) / 60;
+
+  //create String
+  Time = "";
+  if (minutes < 10)
+    Time = Time + 0;
+  Time = Time + minutes;
+  Time = Time + ":";
+  if (seconds < 10)
+    Time = Time + 0;
+  Time = Time + seconds;
+
+  //Clear old Pixels
+  dPrint(90, 110, 2, BAR_BACKGROUND_COLOR, lastTime);
+  //write new Pixels
+  if (minutes >= 20)
+    dPrint(90, 110, 2, RED, Time);
+  else
+    dPrint(90, 110, 2, BLACK, Time);
+  //Set new lastAirCondition
+  lastTime = Time; //Setzt letzten Wert
+}
+
+void dPrint(int y, int x, float scale, int color, String text) {
+  display.setCursor(y, x);
+  display.setTextSize(scale);
+  display.setTextColor(color);
+  display.println(text);
+}
+
+void dPrint(int y, int x, float scale, int color, int text) {
+  display.setCursor(y, x);
+  display.setTextSize(scale);
+  display.setTextColor(color);
+  display.println(text);
 }
