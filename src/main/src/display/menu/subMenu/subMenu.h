59bfa5e7f36cea36fe7cbaa47143b9743d467dbd
OfCores <40722004+OfCores@users.noreply.github.com>
2021-01-15 16:09:15 +0100

    Add files via upload

---
+++ src/main/src/display/menu/subMenu/subMenu.h
@@ -0,0 +1,27 @@
+/*
+  SubMenu.h - SubMenu-Library for Airduino.
+*/
+
+#ifndef SubMenu_h
+#define SubMenu_h
+
+#include "Arduino.h"
+#include "../menuPage/MenuPage.h"
+
+class SubMenu {
+  public:
+    SubMenu(String title, int numberPages, int currentPage, int defaultPage = 1);
+    void init();
+    void handleTouch(TSPoint p);
+
+  private:
+    boolean checkTouch(TSPoint p);
+
+    int _pages;
+    int _currentPage;
+    int _defaultPage;
+    String _title;
+    MenuPage pages[];
+};
+
+#endif
