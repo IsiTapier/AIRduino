b0bc32cac3e62bdb3e1d03b1ed4fcf8dd9065c09
Jones Braun <47891728+RunSimpleTheEA@users.noreply.github.com>
2021-01-22 23:54:40 +0100

    new SD Card lib. added
    some improvements needed but works fine

---
+++ src/main/src/util/SD_Card_Extension.cpp
@@ -0,0 +1,48 @@
+#include "SD_Card_Extension.h"
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include"SD.h"
+#include"SPI.h"
+
+SD_Card_Extension::SD_Card_Extension(int CSPin, String filename)
+: _filename(filename),
+  _CSPin(CSPin),
+  _initialized(false) {
+}
+
+void SD_Card_Extension::initialize() {
+  if(_initialized == false){
+    pinMode(_CSPin, OUTPUT);
+    if (!SD.begin(_CSPin)) {
+      Serial.println("Card failed, or not present");
+      return;
+    }
+    Serial.println("card initialized.");
+    _initialized = true;
+  }
+}
+
+bool SD_Card_Extension::saveValuesToSD(int value1, int value2, int value3, int value4) {
+  String dataString = String(value1)+ "," + String(value2) + "," + String(value3) + "," + String(value4); // convert to CSV (comma seperated values)
+  return saveData(dataString);
+}
+
+bool SD_Card_Extension::saveData(const String & dataString) {
+  initialize();
+  if(SD.exists(_filename)) {
+    File sensorData = SD.open(_filename, FILE_WRITE);
+    if (sensorData) {
+      sensorData.println(dataString); //print data to File
+      sensorData.close(); // close the file
+      return true;
+    }
+  } else {
+    Serial.print("Error writing to file "); Serial.print(_filename);
+    return false;
+  }
+}
---
+++ src/main/src/util/SD_Card_Extension.h
@@ -0,0 +1,25 @@
+/*
+* SD card attached to SPI bus as follows (names after the backslashs are the names of the Airduino Display SD_Card Module Pins):
+** MOSI - pin 11  /DI  (51 MEGA)
+** MISO - pin 12  /DO (50 MEGA)
+** CLK - pin 13   /SCK (52 MEGA)
+** CS - pin 10    /SS  (53 on MEGA)
+*/
+#ifndef SD_Card_Extension_H
+#define SD_Card_Extension_H
+
+class SD_Card_Extension {
+
+public:
+  SD_Card_Extension(int CSPin, String filename);   //Uno 10  for filename please chose sth. like "filename.csv"
+  bool saveValuesToSD(int value1, int value2, int value3, int value4); //insert values to save. If SD Card or File is not working function returns false
+
+private:
+  bool saveData(const String & dataString);
+  void initialize();
+
+  bool _initialized;
+  const String _filename;
+  int _CSPin;
+};
+#endif
